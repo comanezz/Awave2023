19415f6b1f15e02da82431e9ba6a07074176fd06
Aymeric <aymeric.lcharles@gmail.com>
2023-12-25 22:55:07 +0100

    DOMContentLoaded event added with all events added to it. Comment with some doc added

--- script/script.js
+++ script/script.js
@@ -18,52 +18,18 @@ const list = document.getElementById('item-list');
 const img = document.querySelectorAll('.item');
 const itemWidth = img[0].offsetWidth;
 
-// Menu and Shopping bag toggle
-menuButton.addEventListener('click', makeMenuToggle)
-shoppingBag.addEventListener('click', makeMenuToggle)
-shoppingBagFull.addEventListener('click', makeMenuToggle)
-
-// Menu and Shopping bag close
-menuClose.addEventListener('click', () => {
-  menuToggleLeft.classList.remove('menu-toggled');
-})
-
-shoppingClose.forEach(element => {
-  element.addEventListener('click', () => {
-    menuToggleRight.forEach(element => {
-      element.classList.remove('menu-toggled');
-    })
-  })
-})
-
-document.addEventListener('click', closeNavbar)
-
-// Making toggle the list buttons.
-listsMenu.forEach(element => {
-  element.addEventListener('click', makeToggle)
-});
-
-// Making Navbar fixed when scrolling
-window.addEventListener("scroll", makeFixed);
-
-// Making carousel live
-prev.addEventListener('click', () => {
-  list.scrollLeft -= (itemWidth);
-})
-
-next.addEventListener('click', () => {
-  list.scrollLeft += (itemWidth);
-})
-
+// Allow to make the sub-menu buttons toggle 
 function makeToggle(event) {
   const toggleList = document.querySelector('.nav-list');
   const subList = event.target.nextElementSibling
 
+  // Allowing to make the toggle instantly and change the height of the menu selection
   function subListShow(event) {
     const hasWomen = event.target.classList.contains('women');
     const hasMen = event.target.classList.contains('men');
     const hasAboutUs = event.target.classList.contains('about-us');
 
+    // Making the toggle instantly
     toggleList.style.transform = 'translateX(-100%)';
 
     subList.classList.remove('not-visible');
@@ -76,7 +42,8 @@ function makeToggle(event) {
     }
   }
 
-  function subListHidden(event) {
+  // Slide instantly back to the main menu buttons
+  function subListHidden() {
     const returnList = document.querySelectorAll('.return');
 
     returnList.forEach(element => {
@@ -131,4 +98,126 @@ function makeFixed() {
   } else {
     navbar.classList.remove('navbar-fixed');
   }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Menu and Shopping bag toggle
+  menuButton.addEventListener('click', makeMenuToggle)
+  shoppingBag.addEventListener('click', makeMenuToggle)
+  shoppingBagFull.addEventListener('click', makeMenuToggle)
+
+  // Menu and Shopping bag close
+  menuClose.addEventListener('click', () => {
+    menuToggleLeft.classList.remove('menu-toggled');
+  })
+
+  // Loop is need because there is not only one close button. One for mobile and desktop.
+  shoppingClose.forEach(element => {
+    element.addEventListener('click', () => {
+      menuToggleRight.forEach(element => {
+        element.classList.remove('menu-toggled');
+      })
+    })
+  })
+
+  document.addEventListener('click', closeNavbar)
+
+  // Making toggle the list buttons.
+  listsMenu.forEach(element => {
+    element.addEventListener('click', makeToggle)
+  });
+
+  // Making Navbar fixed when scrolling
+  window.addEventListener("scroll", makeFixed);
+
+  // Making carousel live
+  prev.addEventListener('click', () => {
+    list.scrollLeft -= (itemWidth);
+  })
+
+  next.addEventListener('click', () => {
+    list.scrollLeft += (itemWidth);
+  })
+});
+
+// sandeep: Can you add some doc explaining what this does? Looks like this
+// functionality is broken on website as well.
+// function makeToggle(event) {
+// const toggleList = document.querySelector('.nav-list');
+// const subList = event.target.nextElementSibling
+
+// function subListShow(event) {
+// const hasWomen = event.target.classList.contains('women');
+// const hasMen = event.target.classList.contains('men');
+// const hasAboutUs = event.target.classList.contains('about-us');
+
+// toggleList.style.transform = 'translateX(-100%)';
+
+// subList.classList.remove('not-visible');
+// subList.classList.add('visible');
+
+// if (hasWomen || hasMen) {
+// toggleList.style.height = '124px';
+// } else if (hasAboutUs) {
+// toggleList.style.height = '220px';
+// }
+// }
+
+// function subListHidden(event) {
+// const returnList = document.querySelectorAll('.return');
+
+// returnList.forEach(element => {
+// element.addEventListener('click', () => {
+// toggleList.style.transform = 'translateX(0)';
+// subList.classList.remove('visible');
+// subList.classList.add('not-visible');
+// toggleList.style.height = '156px';
+// })
+// })
+// }
+
+// subListShow(event);
+// subListHidden();
+// }
+
+function closeNavbar(event) {
+const menuToggleRightArray = Array.from(menuToggleRight).some(element => {
+element.classList.contains('menu-toggled');
+});
+
+// sandeep: Not sure if you really need to remove and re-attach click event listeners.
+shoppingBag.removeEventListener('click', makeMenuToggle)
+menuButton.removeEventListener('click', makeMenuToggle)
+
+if (!navbar.contains(event.target)) {
+
+menuToggleLeft.classList.remove('menu-toggled');
+menuToggleRight.forEach(element => {
+element.classList.remove('menu-toggled');
+})
+}
+
+if (!menuToggleLeft.classList.contains('menu-toggled') && !menuToggleRightArray) {
+shoppingBag.addEventListener('click', makeMenuToggle);
+menuButton.addEventListener('click', makeMenuToggle);
+}
+}
+
+function makeMenuToggle(event) {
+// sandeep: Why not perform exact comparison?
+if (event.target.id.includes('menu-button')) {
+menuToggleLeft.classList.add('menu-toggled');
+} else if (event.target.id.includes('shopping-bag') || event.target.id.includes('shopping-bag-full')) {
+menuToggleRight.forEach(element => {
+element.classList.add('menu-toggled');
+})
+}
+}
+
+function makeFixed() {
+if (document.documentElement.scrollTop > 0) {
+navbar.classList.add('navbar-fixed');
+} else {
+navbar.classList.remove('navbar-fixed');
+}
 }
